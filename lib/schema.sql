ad526250eb4d2c1b95f4a5f56794256314950013
Scott Hoisington <hoyzington@gmail.com>
2020-01-23 08:05:01 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id TEXT, subgenre_id TEXT);
 
-CREATE TABLE sub_genres (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 
