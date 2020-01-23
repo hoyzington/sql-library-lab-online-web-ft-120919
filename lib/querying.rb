bcb66cda7391e31fbf084cdbf23c96dcfcdf2070
Scott Hoisington <hoyzington@gmail.com>
2020-01-23 07:57:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books JOIN series ON  "
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT  FROM  JOIN  ON  "
+  "SELECT  FROM  JOIN  ON  ;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT  FROM  JOIN  ON  "
+  "SELECT  FROM  JOIN  ON  ;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT  FROM  JOIN  ON  "
+  "SELECT  FROM  JOIN  ON  ;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT  FROM  JOIN  ON  "
+  "SELECT  FROM  JOIN  ON  ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT  FROM  JOIN  ON  "
+  "SELECT  FROM  JOIN  ON  ;"
 end
